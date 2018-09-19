3eb5fa20347657f8968f4a13ee1c3dc84c2a1438
Thomas-Dam-P <tdpe15@student.aau.dk>
2018-09-19 15:53:08 +0200

    Part 2 done

--- Lektion 2 OPGAVER.R
+++ Lektion 2 OPGAVER.R
@@ -15,7 +15,7 @@ prod(1:5)
 #' ### Opgave 2
 is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
 Fibfct = function(n){
-  if (is.wholenumber(n)==FALSE){return("Error, input must be a positive integer")}
+  if (is.wholenumber(n)==FALSE){return("Error, input must be a positive integer")} #alternativt if (n!=round(n))
   if (n<1){return("Error, input must be a positive integer")}
   if (n==1) {return(1)}
   if (n==2) {return(1)}
@@ -33,6 +33,34 @@ Fibfct(54)
 #' ### Opgave 3
 #install.packages("ggplot2")
 #library(ggplot2)
+#?ggplot2
 
 #' ## Part 2
 #' ### Opgave 1
+
+head(cars)
+X=cars[,1]
+y=cars[,2]
+B=solve((t(X)%*%X),t(X)%*%y)
+plot(cars)
+abline(0,B)
+varians=(t(y-X%*%B)%*%(y-X%*%B))/(50-2)
+print(varians)
+
+#' ### Opgave 2
+X2=matrix(c(X,X^2),50,2)
+B=solve((t(X2)%*%X2),t(X2)%*%y)
+plot(cars)
+curve(B[1]*x+B[2]*x^2,add=TRUE)
+varians=(t(y-X2%*%B)%*%(y-X2%*%B))/(50-2)
+print(varians)
+#' ### Opgave 3
+head(trees)
+Girth=trees[,1]
+Height=trees[,2]
+X=matrix(c(rep(1,31),Girth,Height),31,3)
+y=trees[,3]
+B=solve((t(X)%*%X),t(X)%*%y)
+var=(t(y-X%*%B)%*%(y-X%*%B))/(31-3)
+print(B)
+print(var)
